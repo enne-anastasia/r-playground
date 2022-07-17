7562fab64f3fe96942593830d781f65fcaed9166
enne-anastasia <30407603+enne-anastasia@users.noreply.github.com>
2022-07-17 18:54:34 +0200

    Update dataVisualisationExercises.r

--- dataVisualisationExercises.r
+++ dataVisualisationExercises.r
@@ -37,3 +37,62 @@ ggplot(data = mpg) +
 
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
+
+?mpg
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ,y=hwy,color=displ<5))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ,y=hwy)) +
+  facet_wrap(~ class, nrow=2)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ,y=hwy)) +
+  facet_grid(drv ~ cyl)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = drv, y = cyl))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(drv ~ .)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(. ~ cyl)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(~ class, nrow = 2)
+
+ggplot(data = mpg) +
+  geom_point(mapping=aes(x=displ,y=hwy))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping=aes(x=displ,y=hwy))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x=displ,y=hwy,linetype=drv))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x=displ,y=hwy,group=drv))
+
+ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x=displ,y=hwy,color=drv),show.legend = FALSE)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ,y=hwy)) +
+  geom_smooth(mapping = aes(x=displ,y=hwy))
+
+ggplot(data = mpg,mapping = aes(x=displ,y=hwy)) +
+  geom_point() +
+  geom_smooth()
+
+ggplot(data = mpg,mapping = aes(x=displ,y=hwy)) +
+  geom_point(mapping = aes(color=class)) +
+  geom_smooth()
+
+ggplot(data = mpg,mapping = aes(x=displ,y=hwy)) +
+  geom_point(mapping = aes(color=class)) +
+  geom_smooth(data=filter(mpg,class == "subcompact"),se=FALSE)
